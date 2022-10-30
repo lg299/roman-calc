188e4cfb0328c3e062c218944355ec82eaff1cfb
Liz Griffin <liz@spiritai.com>
2022-10-30 18:34:12 +0000

    Added table for numbers, added todo file

--- src/main.ts
+++ src/main.ts
@@ -4,36 +4,64 @@ import blessed from 'blessed';
 export function main() {
   let screen = blessed.screen({
     smartCSR: true,
-    dockBorders: true
+    dockBorders: true,
   });
 
 
-  let box = blessed.box({
-    parent: screen,
-    top: 'center',
-    left: 'center',
-    width: '20%',
-    height: '80%',
-    content: '',
-    border: {
-      type: 'line',
-    },
-  });
+  // let box = blessed.box({
+  //   parent: screen,
+  //   top: 'center',
+  //   left: 'center',
+  //   width: '20%',
+  //   height: '80%',
+  //   content: '',
+  //   border: {
+  //     type: 'line',
+  //   },
+  // });
 
   let topBox = blessed.box({
     parent: screen,
     top: 0,
     left: 'center',
     width: '20%',
-    height: '20%',
-    content: 'Enter a number',
+    height: '10%',
+    content: '0',
     border: {
       type: "line",
     },
   });
 
-  screen.append(box);
+  // screen.append(box);
   screen.append(topBox);
+
+
+  var table = blessed.listtable({
+    parent: screen,
+    headers: ['1', '2', '3','4'],
+    padding: {left: 0, right: 0},
+    top: '5%',
+    left: 'center',
+    border: 'line',
+    align: 'center',
+    keys: true,
+    width: '20%',
+    height: '18%',
+    vi: false,
+    name: 'table',
+  });
+
+  // Focus table, and render results to screen
+  table.setData(
+    [
+      ['AC', '±', '%', '÷'],
+      ['7', '8', '9', 'x'],
+      ['4', '5', '6', '+'],
+      ['1', '2', '3', '-'],
+      ['@', '0', '.', '=']
+    ],
+  );
+  table.focus();
 
   screen.key(['escape', 'q', 'C-c'], function () {
     return process.exit(0);
